3981674c918e8c6f8864f6f6bcf884dcff49ec9c
Kimish Patel <kimishpatel@fb.com>
2023-08-30 17:39:01 -0700

    [Quantization] Add metadata porting for nodes added by quantization
    Summary:
    This diff adds adding metadata to q-dq nodes by inferring the
    quatization intent from node annotations. Annotations on the node are
    way for user to specify how a node or subgraph is supposed to be
    quantized. We continue to use that information to copy metadata on Q/DQ
    node from appropriate nodes.
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: e1fa2a661e99d5b7339fc0c85e626ba03c64b570
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/107107

--- torch/ao/quantization/quantize_pt2e.py
+++ torch/ao/quantization/quantize_pt2e.py
@@ -30,6 +30,7 @@ from typing import Any, Tuple
 
 from torch.fx.passes.infra.pass_manager import PassManager
 from torch.ao.quantization.pt2e.duplicate_dq_pass import DuplicateDQPass
+from torch.ao.quantization.pt2e.port_metadata_pass import PortNodeMetaForQDQ
 
 __all__ = [
     "prepare_pt2e",
@@ -106,6 +107,11 @@ def convert_pt2e(
     pm = PassManager([DuplicateDQPass()])
     model = pm(model).graph_module
 
+    print(model)
+    for node in model.graph.nodes:
+        print(node, node.meta)
+    pm = PassManager([PortNodeMetaForQDQ()])
+    model = pm(model).graph_module
     if use_reference_representation:
         model = reference_representation_rewrite(model)
 
